36fc1f1ef76a7ecf3c95fe0e01ee77521eeb7ca8
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-14 00:37:59 -0300

    Initial Commit

---
+++ estruturaDados-02-linked_list/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  estruturaDados-02-linked_list
+//
+//  Created by Luiz Carlos da Silva Araujo on 14/08/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
